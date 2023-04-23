287b415669d15d93a86727c8fbba1ba12779fc7a
ElianCruz <ecruz@outcoding.com>
2023-04-23 18:07:19 -0400

    refactorizing - adding EF core and its configuration

---
+++ Sat.Recruitment.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sat.Recruitment.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
