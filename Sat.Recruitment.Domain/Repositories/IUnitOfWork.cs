f9f1b47ea720a7ba8af5d3dbedeb39125d09e1c0
ElianCruz <ecruz@outcoding.com>
2023-04-23 18:41:08 -0400

    refactorizing - adding unit of work to handle a common save place fro multi repositories

---
+++ Sat.Recruitment.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sat.Recruitment.Domain.Repositories
+{
+    public interface IUnitOfWork
+    {
+        Task SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Sat.Recruitment.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Sat.Recruitment.Domain.Repositories;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sat.Recruitment.Infrastructure
+{
+    internal sealed class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
