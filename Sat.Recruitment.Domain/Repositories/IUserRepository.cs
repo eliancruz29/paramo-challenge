60702f69c94fff698315e564eb85bd4a47c47728
ElianCruz <ecruz@outcoding.com>
2023-04-23 18:54:28 -0400

    refactorizing - adding the repository for handle the user access to database

---
+++ Sat.Recruitment.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Sat.Recruitment.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sat.Recruitment.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+
+        Task<IEnumerable<User>> GetByNameAsync(string name, CancellationToken cancellationToken = default);
+
+        Task AddAsync(User user, CancellationToken cancellationToken = default);
+
+        void RemoveAsync(User user);
+    }
+}
---
+++ Sat.Recruitment.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sat.Recruitment.Domain.Entities;
+using Sat.Recruitment.Domain.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sat.Recruitment.Infrastructure.Repositories
+{
+    internal sealed class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<User> GetByEmailAsync(string email, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Users
+                            .FirstOrDefaultAsync(u => email == u.Email, cancellationToken);
+        }
+
+        public async Task<IEnumerable<User>> GetByNameAsync(string name, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Users
+                            .Where(u => name == u.Name)
+                            .ToListAsync(cancellationToken);
+        }
+
+        public async Task AddAsync(User user, CancellationToken cancellationToken = default)
+        {
+            await _dbContext.Users.AddAsync(user, cancellationToken);
+        }
+
+        public void RemoveAsync(User user)
+        {
+            _dbContext.Users.Remove(user);
+        }
+    }
+}
