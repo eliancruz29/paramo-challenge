44f8c267289abd4c5d3d56894e0cfeecfc3f87d6
ElianCruz <ecruz@outcoding.com>
2023-04-23 18:35:39 -0400

    refactorizing - adding the migrations in creating the database

---
+++ Sat.Recruitment.Infrastructure/Configurations/UserConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sat.Recruitment.Domain.Entities;
+using System;
+
+namespace Sat.Recruitment.Infrastructure.Configurations
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.Name).IsRequired().HasMaxLength(100);
+
+            builder.Property(u => u.Email).IsRequired().HasMaxLength(250);
+
+            builder.Property(u => u.Address).IsRequired().HasMaxLength(250);
+
+            builder.Property(u => u.Phone).IsRequired().HasMaxLength(25);
+
+            builder.Property(u => u.UserType)
+                .HasConversion(
+                    ut => ut.ToString(),
+                    ut => (UserType)Enum.Parse(typeof(UserType), ut));
+
+            builder.HasIndex(u => u.Name);
+
+            builder.HasIndex(u => u.Email).IsUnique();
+        }
+    }
+}
---
+++ Sat.Recruitment.Infrastructure/Migrations/20230423222919_Create_Database.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sat.Recruitment.Infrastructure.Migrations
+{
+    public partial class Create_Database : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 100, nullable: true),
+                    Email = table.Column<string>(maxLength: 250, nullable: true),
+                    Address = table.Column<string>(maxLength: 250, nullable: true),
+                    Phone = table.Column<string>(maxLength: 25, nullable: true),
+                    UserType = table.Column<string>(nullable: false),
+                    Money = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Email",
+                table: "User",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Name",
+                table: "User",
+                column: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
