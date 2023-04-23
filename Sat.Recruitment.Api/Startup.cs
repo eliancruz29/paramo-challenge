a2ddce70573cde348c0c1ad3e087c0df6e225883
ElianCruz <ecruz@outcoding.com>
2023-04-23 14:59:57 -0400

    refactorizing - add dependency injection for projects

--- Sat.Recruitment.Api/Startup.cs
+++ Sat.Recruitment.Api/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Sat.Recruitment.Application;
+using Sat.Recruitment.Infrastructure;
 
 namespace Sat.Recruitment.Api
 {
@@ -26,6 +21,11 @@ namespace Sat.Recruitment.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services
+                .AddApplication()
+                .AddInfrastructure();
+
             services.AddSwaggerGen();
         }
 
