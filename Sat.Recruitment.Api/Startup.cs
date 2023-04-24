b21dbb7925904c0f77c1ba0fc063dab29d15b79b
ElianCruz <ecruz@outcoding.com>
2023-04-23 21:19:52 -0400

    refactorizing - adding model validations and custom error model

--- Sat.Recruitment.Api/Startup.cs
+++ Sat.Recruitment.Api/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Sat.Recruitment.Application;
 using Sat.Recruitment.Infrastructure;
 
@@ -22,11 +24,15 @@ namespace Sat.Recruitment.Api
         {
             services.AddControllers();
 
+            services.Configure<ApiBehaviorOptions>(options
+                => options.SuppressModelStateInvalidFilter = true);
+
             services
                 .AddApplication()
                 .AddInfrastructure(Configuration);
 
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API V1", Version = "v1" }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,18 +41,18 @@ namespace Sat.Recruitment.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                });
             }
-            app.UseSwagger();
-
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c =>
+            else
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-            });
-            app.UseRouting();
+                app.UseExceptionHandler("/error");
+            }
 
-            app.UseAuthorization();
+            app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
---
+++ Sat.Recruitment.Domain/Shared/Result.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Sat.Recruitment.Domain.Shared
+{
+    public class Result
+    {
+        protected internal Result(bool isSuccess, string msg)
+        {
+            if (string.IsNullOrWhiteSpace(msg))
+            {
+                throw new InvalidOperationException();
+            }
+
+            IsSuccess = isSuccess;
+            Message = msg;
+        }
+
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public string Message { get; }
+
+        public static Result Success(string msg) => new Result(true, msg);
+        
+        public static Result Error(string msg) => new Result(false, msg);
+
+        public static Result<TValue> Success<TValue>(TValue value, string msg) => new Result<TValue>(value, true, msg);
+
+        public static Result<TValue> Error<TValue>(TValue value, string msg) => new Result<TValue>(value, false, msg);
+    }
+}
